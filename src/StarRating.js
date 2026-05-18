c58bb7027ca9cdfc4b8c802fe9e69e3231080695
Raj Shukla <rajshukla9630@gmail.com>
2026-05-18 09:34:40 +0530

    L-120 proptypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -28,14 +28,18 @@ export default function StarRating({maxRating = 5,
     const [rating, setRating] = useState(defaultRating);
     const [tempRating, setTempRating] = useState(0);
     const ratingMessage = tempRating ? message[tempRating-1] : message[rating-1];
+
+    function handleSetRating(rating){
+        setRating(rating)
+        if(onSetRating){
+            onSetRating(rating);
+        }
+    }
     return <div style={containerStyle}>
         <div style={startContainerStyle}>
             {Array.from( {length:maxRating}, (_,i) => (
                 <Star key={i} 
-                onRate={()=>{
-                    setRating(i+1)
-                   onSetRating(i+1);
-                }}
+                onRate={()=>handleSetRating(i+1)}
                 onHoverIn={()=>setTempRating(i+1)}
                 onHoverOut={()=>setTempRating(0)}
                 full={tempRating ? tempRating > i :  rating > i}
